7bf1137088c11809ed08f240de24b13d74058ae7
HARSH VARDHAN <harshvardhan.bitm@gmail.com>
2024-05-21 15:00:20 +0530

    adding new feature called calculator

--- Main.java
+++ Main.java
@@ -2,5 +2,9 @@ public class Main {
 
     public  static void main(String[] args){
         System.out.println("Hello World");
+
+        System.out.println("Addition" + Calculator.add(10, 23));
     }
+
+
 }
