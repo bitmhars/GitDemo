b7a33fe5d04344aaf1c6d409746105373b5e554c
HARSH VARDHAN <harshvardhan.bitm@gmail.com>
2024-05-21 15:26:27 +0530

    updated calculator

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main {
     public  static void main(String[] args){
         System.out.println("Hello World");
 
-        System.out.println("Addition" + Calculator.add(10, 23));
+        System.out.println("Addition " + Calculator.add(10, 23));
     }
 
 
